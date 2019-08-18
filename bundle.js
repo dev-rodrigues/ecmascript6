d30b0086f713844469992ba7c4f7ed6b9eb2ad6e
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 18:38:13 -0300

    usando o rest

--- bundle.js
+++ bundle.js
@@ -1,26 +1,28 @@
-"use strict";
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
 // REST
 // PEGA O RESTO DAS PROPRIEDADES
-var usuario = {
-  nome: "carlos",
-  idade: 24,
-  empresa: "AVANADE"
-};
-
-var nome = usuario.nome,
-    resto = _objectWithoutProperties(usuario, ["nome"]);
-
-console.log(nome);
-console.log(resto);
-var arr = [1, 2, 3, 4, 5];
-var a = arr[0],
-    b = arr[1],
-    c = arr.slice(2);
-console.log(a);
-console.log(b);
-console.log(c);
+// const usuario = {
+//     nome: "carlos",
+//     idade: 24,
+//     empresa: "AVANADE"
+// }
+// const { nome, ...resto } = usuario
+// console.log(nome);
+// console.log(resto);
+// const arr = [1,2,3,4,5];
+// const [ a, b, ...c] = arr;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// function soma(a, b, c) {
+//     return a + b + c;
+// }
+// console.log(soma(1,2, 3));
+// function newSoma(...params) {
+//     return params.reduce((total, next) => total + next);
+// }
+// console.log(newSoma(1,2, 3));
+// function newSoma2(a, b, ...params) {
+//     return params;
+// }
+// console.log(newSoma2(1,2, 3));
+"use strict";
