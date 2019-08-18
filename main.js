c0974ab6cc6e05b5e54b18c6c9b824eee0078ac0
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 19:18:40 -0300

    configurando o webpack -> exportando funcionando

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-// usando object short syntax
-// quando o nome da variavel é igual ao nome do atributo
-// não precisa repetir :)
-const nome = "carlos henrique";
-const idade = 24;
+import { soma } from './funcoes';
 
-const usuario = {
-    nome,
-    idade,
-    empresa: "AVANADE",
-}
+console.log(soma(1,2));
+
