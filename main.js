6e20de3ff01c60eba73b91cec452f0427f97407b
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 16:39:17 -0300

    usando metodos staticos

--- main.js
+++ main.js
@@ -1,32 +1,8 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
-
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
+class Matematica {
+    static soma(a, b) {
+        return a + b;
     }
 }
 
-
-// conceito de herança
-class TodoList extends List{
-    constructor() {
-        super();
-
-        this.usuario = "carlos henrique";
-    }
-
-    monstraUsuario() {
-        console.log(this.usuario);
-    }
-}
-
-var MinhaLista = new TodoList();
-
-document.getElementById('novoTodo').onclick = function() {
-    MinhaLista.add('Estou usando herança');
-}
-
-MinhaLista.monstraUsuario();
+// metodos estaticos precisam retornar algum valor
+console.log(Matematica.soma(1,2));
