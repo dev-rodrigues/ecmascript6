0319cd90d478b4faf98854e1d4ed43796dc6555c
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 16:52:04 -0300

    diferenca entre const e let

--- bundle.js
+++ bundle.js
@@ -1,27 +1,31 @@
+// constantes não podem ter seu valor reatribuído;
+// const = read-only, ou seja, após definido o seu valor
+// constantes permitem somente leitura;
+// const a = 1;
+// console.log(a);
+// a = 3;
+// console.log(a);
+////////////////////////////////////////////////////////////
+// podemos mutar uma constante
+// const usuario = {
+//     nome: "carlos henrique"
+// }
+// console.log(usuario.nome);
+// usuario.nome = "Diego";
+// console.log(usuario.nome);
+////////////////////////////////////////////////////////////
+// let tem seu valor visivel dentro do escopo da função
+// function teste(x) {
+//     let y = 2;
+//     console.log(y);
+//     if (x > 5) {
+//         // variavel y pode ser acessada daqui
+//         let y = 4;
+//         console.log(x, y);
+//     }
+// }
+// // variavel y não é acessivel daqui
+// //  console.log(y);
+// console.log(teste(10));
+////////////////////////////////////////////////////////////
 "use strict";
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Matematica =
-/*#__PURE__*/
-function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}(); // metodos estaticos precisam retornar algum valor
-
-
-console.log(Matematica.soma(1, 2));
--- main.js
+++ main.js
@@ -1,8 +1,35 @@
-class Matematica {
-    static soma(a, b) {
-        return a + b;
-    }
-}
+// constantes não podem ter seu valor reatribuído;
+// const = read-only, ou seja, após definido o seu valor
+// constantes permitem somente leitura;
+// const a = 1;
+// console.log(a);
+// a = 3;
+// console.log(a);
 
-// metodos estaticos precisam retornar algum valor
-console.log(Matematica.soma(1,2));
+////////////////////////////////////////////////////////////
+// podemos mutar uma constante
+// const usuario = {
+//     nome: "carlos henrique"
+// }
+// console.log(usuario.nome);
+
+// usuario.nome = "Diego";
+// console.log(usuario.nome);
+
+////////////////////////////////////////////////////////////
+// let tem seu valor visivel dentro do escopo da função
+// function teste(x) {
+//     let y = 2;
+//     console.log(y);
+    
+//     if (x > 5) {
+//         // variavel y pode ser acessada daqui
+//         let y = 4;
+//         console.log(x, y);
+        
+//     }
+// }
+// // variavel y não é acessivel daqui
+// //  console.log(y);
+// console.log(teste(10));
+////////////////////////////////////////////////////////////
