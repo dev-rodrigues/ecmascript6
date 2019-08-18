aa9ebe7de7a702c671350e922413a05a7132e506
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 18:49:58 -0300

    usando object short syntax

--- bundle.js
+++ bundle.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// usando object short syntax
+// quando o nome da variavel é igual ao nome do atributo
+// não precisa repetir :)
+var nome = "carlos henrique";
+var idade = 24;
+var usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: "AVANADE"
+};
---
+++ main.js
@@ -0,0 +1,11 @@
+// usando object short syntax
+// quando o nome da variavel é igual ao nome do atributo
+// não precisa repetir :)
+const nome = "carlos henrique";
+const idade = 24;
+
+const usuario = {
+    nome,
+    idade,
+    empresa: "AVANADE",
+}
