83b714c747dff847496ff83d9bd4ffa136d8e6b9
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 18:47:39 -0300

    limpando

--- bundle.js
+++ bundle.js
@@ -1,5 +1,1 @@
 "use strict";
-
-var nome = "carlos henrique";
-var idade = 24;
-console.log("meu nome \xE9 ".concat(nome, " e minha idade \xE9 ").concat(idade));
--- main.js
+++
@@ -1,4 +0,0 @@
-const nome = "carlos henrique"
-const idade = 24;
-
-console.log(`meu nome é ${nome} e minha idade é ${idade}`);
