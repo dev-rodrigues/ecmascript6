dbb545e999c7450c913bc481c3ec01227f8325e8
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 16:31:18 -0300

    usando herança

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-class TodoList {
+class List {
     constructor() {
-        this.todos = [];
-
+        this.data = [];
     }
 
-    addTodo() {
-        this.todos.push('novo todo');
-        console.log(this.todos);
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+        
     }
+}
+
+
+// conceito de herança
+class TodoList extends List{
+
 }
 
 const MinhaLista = new TodoList();
 
 document.getElementById('novoTodo').onclick = function() {
-    MinhaLista.addTodo();
+    MinhaLista.add('Estou usando herança');
 }
