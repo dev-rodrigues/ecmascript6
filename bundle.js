d1eb6cd4928e9a28bb97bbc6ff3d3e7bee740705
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-08-18 18:44:22 -0300

    usando spread

--- bundle.js
+++ bundle.js
@@ -1,1 +1,23 @@
 "use strict";
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// SPREAD
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
+var usuario = {
+  nome: "carlos henrique",
+  idade: 23
+};
+
+var newUsuario = _objectSpread({}, usuario, {
+  nome: "fernadno"
+});
+
+console.log(newUsuario);
---
+++ main.js
@@ -0,0 +1,19 @@
+// SPREAD
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+const usuario = {
+    nome: "carlos henrique",
+    idade: 23
+};
+
+const newUsuario = {...usuario, nome: "fernadno"};
+
+console.log(newUsuario);
+
+
+
+
